f3053c956fe094599199f9266fbd0fc8450e1522
Georgios Markomanolis <georgios.markomanolis@csc.fi>
2021-02-22 01:46:52 +0200

    saxpy example

---
+++ porting/codes/saxpy/cuda/sub.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name=example
+#SBATCH --account=4training
+#SBATCH --reservation=gpu_training
+#SBATCH --partition=gpu
+#SBATCH --time=00:05:00
+#SBATCH -e error_%j
+#SBATCH -o out_%j
+#SBATCH --ntasks=1
+#SBATCH --mem-per-cpu=8000
+#SBATCH --gres=gpu:v100:1
+
+
+srun ./saxpy
